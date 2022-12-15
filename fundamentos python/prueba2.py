d9f631b8636427e7b1f2f70899cefbf55505841b
bennnnja <benja_1814@hotmail.com>
2022-12-14 23:18:08 -0300

    Primer Avance

--- fundamentos python/prueba2.py
+++ fundamentos python/prueba2.py
@@ -5,7 +5,22 @@ import string
 class Home:
     @cherrypy.expose
     def index(self):
-        return"<a href='/links'>Rutas</a"     
+        return"""
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <h1 align="center"> FORO </h1>
+        <div align="center">
+        </div>
+        <div align="center">
+        <img src="https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green">
+        </div>
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <ul>
+        <h1 align="center"><a href='/links'>Bienvenido a FORO</a</h1>  
+        """
     
     @cherrypy.expose
     def links(self):
@@ -27,24 +42,37 @@ class Home:
             <li><a href='/contact'>Contact</a></li>
             <li><a href='/information'>Information</a></li>
             <li><a href='/generate'>Generate</a></li>
-            <li><a href='/icci'>Icci</a></li>
+            <li><a href="https://www.icci-unap.cl/">Ir a Icci UNAP
             
         """ 
 
     @cherrypy.expose
     def contact(self):
-        return"<a href='/links'>Rutas</a" 
-    
-    @cherrypy.expose
-    def icci(self):
-        return "https://www.inf.unap.cl/index.php/tag/unap/"     
-
+        return"""
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <h1 align="center"> CONTACTOS </h1>
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <ul>
+        """ 
     @cherrypy.expose
     def information(self):
-        return"<a href='/links'>Rutas</a"
+        return"""
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <h1 align="center"> INFORMACION </h1>
+        <div align="center">
+        </div>
+        <h1 align="center">=========================================================================== </h1>
+        <ul>
+        """
 
     @cherrypy.expose
     def generate(self, length=10):
-        return''.join(random.sample(string.hexdigits, int(length)))  
+        return''.join(random.sample(string.hexdigits, int(length)))
 
 cherrypy.quickstart(Home())         
